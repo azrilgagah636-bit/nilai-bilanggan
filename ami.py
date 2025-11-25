6d0afae88efee33947d713935bd1d38ea271ddd6
azrilgagah636-bit <azrilgagah636@gmail.com>
2025-11-24 20:24:04 -0800

    Add number breakdown functionality in ami.py

---
+++ ami.py
@@ -0,0 +1,54 @@
+try:
+    nilai = int(input("masukkan angka:"))
+
+    if 0<= nilai <= 9999999:
+       jutaan = nilai // 1000000
+       sisa_jutaan = nilai  % 1000000
+
+       ratusan_ribu = sisa_jutaan // 100000
+       sisa_ratusan_ribu = sisa_jutaan % 100000
+
+       puluhan_ribu = sisa_ratusan_ribu // 10000
+       sisa_puluhan_ribu = sisa_ratusan_ribu % 10000
+
+       ribuan = sisa_puluhan_ribu // 1000
+       sisa_ribuan = sisa_puluhan_ribu % 1000
+
+       ratusan = sisa_ribuan // 100
+       sisa_ratusan = sisa_ribuan % 100
+    
+
+       puluhan = sisa_ratusan // 10
+       sisa_puluhan = sisa_ratusan % 10
+
+       satuan = sisa_puluhan // 1
+       sisa_satuan = sisa_puluhan % 1
+
+       print("\nhasil pemecahan angka:")
+   
+       if nilai >= 1000000:
+           print(f"{jutaan} merupakan jutaan")
+
+       if nilai >= 100000:
+           print(f"{ratusan_ribu} merupakan ratusan ribu")
+
+       if nilai >= 10000:
+           print(f"{puluhan_ribu} merupakan puluahn ribu")
+
+       if nilai >= 1000:
+           print(f"{ribuan} merupakan ribuan")
+
+       if nilai >= 100:
+           print(f"{ratusan} merupakan ratusan")
+
+       if nilai >= 10:
+           print(f"{puluhan} merupakan puluhan")
+
+       if nilai >= 1:
+           print(f"{satuan} merupakan satuan")
+         
+      
+    else:
+         print("masukkan ulang")
+except ValueError:
+    print("tidak valid")
